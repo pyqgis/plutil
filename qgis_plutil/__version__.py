f9b10d9acb5cd61703ee4f69bbd2c0a1e79275f9
Nicu Tofan <nicu.tofan@gmail.com>
2019-11-16 20:12:36 +0200

    remove qgis dependency

--- qgis_plutil/__version__.py
+++ qgis_plutil/__version__.py
@@ -4,6 +4,6 @@ The version of this package. It is read by setup.py.
 """
 major = 0
 minor = 1
-patch = 0
+patch = 1
 
 __version__ = '%d.%d.%d' % (major, minor, patch)
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = None
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'qgis',
+    # 'qgis', There is no such package
     'PyQt5',
 ]
 
