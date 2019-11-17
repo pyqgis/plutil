d3acbaf0f5deeabc8c57f248335f4074320c7af2
Nicu Tofan <nicu.tofan@gmail.com>
2019-11-17 22:48:59 +0200

    Test for plugin base class

--- tests/unit/test_plugin.py
+++ tests/unit/test_plugin.py
@@ -13,16 +13,25 @@ from unittest import TestCase, SkipTest
 from unittest.mock import MagicMock
 
 from qgis_plutil.plugin import PlUtilPlugin
+from qgis.gui import QgisInterface
 
 logger = logging.getLogger('tests.plugin')
 
 
 class TestTestee(TestCase):
     def setUp(self):
-        self.testee = PlUtilPlugin()
+        self.iface = MagicMock(spec=QgisInterface)
+        self.plugin_dir = tempfile.mkdtemp(suffix=None, prefix=None)
+        self.testee = PlUtilPlugin(
+            iface=self.iface, plugin_name="test_plugin",
+            plugin_dir=self.plugin_dir)
 
     def tearDown(self):
+        if os.path.isdir(self.plugin_dir):
+            shutil.rmtree(self.plugin_dir)
         self.testee = None
 
     def test_init(self):
-        self.testee = PlUtilPlugin()
+        self.testee = PlUtilPlugin(
+            iface=None, plugin_name=None,
+            plugin_dir=None)
